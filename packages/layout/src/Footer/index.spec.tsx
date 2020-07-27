f509b298a996ee3684f1fe013bebb20b80e2b69c
Hassan Khan <hassan.khan@endemolshine.com>
2020-07-27 01:54:24 +0100

    refactor(layout): make `<Footer/>` component more flexible

--- packages/layout/src/Footer/index.spec.tsx
+++ packages/layout/src/Footer/index.spec.tsx
@@ -5,12 +5,9 @@ import { Footer } from './';
 
 describe('<Footer />', () => {
   it('renders correctly', () => {
-    const { getByText } = render(
-      <Footer tagline="Spartan UI" version="1.0.0" />,
-    );
-    const copyrightElement = getByText(/spartan/i);
-    expect(copyrightElement).toBeInTheDocument();
-    const versionElement = getByText(/1\.0\.0/i);
-    expect(versionElement).toBeInTheDocument();
+    const { getByText } = render(<Footer>Footer</Footer>);
+
+    const footerElement = getByText('Footer');
+    expect(footerElement).toHaveClass('footer');
   });
 });
--- packages/layout/src/Footer/index.tsx
+++ packages/layout/src/Footer/index.tsx
@@ -2,20 +2,8 @@ import React from 'react';
 
 import styles from './styles.module.scss';
 
-export type FooterProps = {
-  tagline: string;
-  version: string;
-};
+export type FooterProps = {};
 
 export const Footer: React.FC<FooterProps> = (props) => {
-  return (
-    <footer className={styles.wrapper}>
-      <div className={styles.tagline}>
-        <span>{props.tagline}</span>
-      </div>
-      <div className={styles.version}>
-        <span>{props.version}</span>
-      </div>
-    </footer>
-  );
+  return <footer className={styles.footer}>{props.children}</footer>;
 };
